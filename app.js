28edebe9e3e8c42e20c6b29a79aeb8dbe9e8ca32
sukaynaghnemat <sukaynaghnemat@yahoo.com>
2021-02-11 15:57:22 +0200

    add bransh

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
@@ -10,8 +10,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) {
   //eslint-disable-line
-  let z = a+b;
-  return [z,`The sum of ${a} and ${b} is ${z}.`];
+  let z = a + b;
+  return [z, `The sum of ${a} and ${b} is ${z}.`];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -31,11 +31,11 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) {
   let z = a * b;
   //eslint-disable-line
-  return [z,`The product of ${a} and ${b} is ${z}.`];
+  return [z, `The product of ${a} and ${b} is ${z}.`];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -53,10 +53,23 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   //eslint-disable-line
-}
 
+  let sum1 = sum(a, b);
+  let sum2 = sum(sum1[0], c);
+  let mult1 = multiply(a, b);
+  let mult2 = multiply(mult1[0], c);
+  console.log(sum2, mult2[0]);
+  console.log("4 and 7 and 5 sum to 16.")
+ // console.log()
+  return [
+    sum2[0],
+    mult2[0],
+    `${a} and ${b} and ${c} sum to ${sum2[0]}.`,
+    `The product of ${a} and ${b} and ${c} is ${mult2[0]}.`,
+  ];
+}
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
